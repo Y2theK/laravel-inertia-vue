857c134601f54ad266499a5537852763a8b007d1
Ye Yint Kyaw <87354517+Y2theK@users.noreply.github.com>
2022-11-06 15:45:34 +0630

    go admin route and admin dashboard after login

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
-
+//admin rotues
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
+])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });
